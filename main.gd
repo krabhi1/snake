9fd6b2fc5f254b262aee2967aab9005d36ec50c0
krabhi1 <abhishek33gl@gmail.com>
2024-10-29 23:43:57 +0530

    added design of difficulty dialog

--- main.gd
+++ main.gd
@@ -7,6 +7,9 @@ func _on_play_pressed() -> void:
 
 
 func _on_difficulty_pressed() -> void:
+	#open the difficulty menu
+	$DifficultyDialog.visible = true
+	
 	
 	pass # Replace with function body.
 
